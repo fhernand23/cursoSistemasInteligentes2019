c89a9694ea6119b294bd3741c24e6726923649cb
fhernandez <fede.herz@gmail.com>
2019-05-02 15:31:15 -0300

    - rectangle classifier

--- rectangles-clasiffier.py
+++ rectangles-clasiffier.py
@@ -3,14 +3,16 @@ from matplotlib import pyplot
 import matplotlib.patches as patches
 from pandas import DataFrame
 import numpy as np
+from collections import defaultdict
 
 # generate random 2d classification dataset
-X, y = make_blobs(n_samples=20, centers=2, n_features=2)
+X, y = make_blobs(n_samples=40, centers=2, n_features=2)
 # scatter plot, dots colored by class value
 df = DataFrame(dict(x=X[:,0], y=X[:,1], label=y))
 colors = {0:'green', 1:'blue'}
 fig, ax = pyplot.subplots()
 grouped = df.groupby('label')
+newValues = defaultdict();
 # plot points and rectangles
 for key, group in grouped:
     # add points
@@ -33,6 +35,18 @@ for key, group in grouped:
         fill=False,
         zorder=2
       ))
+    # add new values
+    newValues[key] = [np.random.normal(x_mean, x_std, 3), np.random.normal(y_mean, y_std, 3)]
+
+# suggest 3 more values for every classification
+print(newValues[0])
+print(newValues[1])
+pyplot.scatter(newValues[0][0][0], newValues[0][1][0],s=50)
+pyplot.scatter(newValues[0][0][1], newValues[0][1][1],s=50)
+pyplot.scatter(newValues[0][0][2], newValues[0][1][2],s=50)
+pyplot.scatter(newValues[1][0][0], newValues[1][1][0],s=50)
+pyplot.scatter(newValues[1][0][1], newValues[1][1][1],s=50)
+pyplot.scatter(newValues[1][0][2], newValues[1][1][2],s=50)
 
 # show plot
 pyplot.show()
